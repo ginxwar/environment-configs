9fc4629df87a4fea8ec9213286c90c068edd15bf
Kevin Wu <kevin.wu@gmail.com>
2014-08-05 11:28:31 -0700

    added mounting points for web-d

--- tmux/web-d1.sh
+++ tmux/web-d1.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 #/Users/kevinwu/scripts/web-d1.sh
- 
+
+if [ ! -d "/Volumes/web-d" ]
+then
+  mkdir /Volumes/web-d
+  sshfs kevinwu@web-d1.identity.ucsb.edu:/local/www/ /Volumes/web-d
+
+  # open up sublime
+  cd /Volumes/web-d
+  subl .
+fi
+
 SESSIONNAME="web-d1"
 tmux has-session -t $SESSIONNAME $> /dev/null
- 
+
 if [ $? != 0 ]
-	then
-		# new session with name $SESSIONNAME and window 0 named "base"
-		tmux new-session -s $SESSIONNAME -n base -d
+        then
+                # new session with name $SESSIONNAME and window 0 named "base"
+                tmux new-session -s $SESSIONNAME -n base -d
 
-		# commands
-		tmux send-keys 'ssh kevinwu@util-d.identity.ucsb.edu' 'C-m'
-		tmux send-keys 'tail -f /local/syslog/identity-info.log' 'C-m'
+                # commands
+                tmux send-keys 'ssh kevinwu@util-d.identity.ucsb.edu' 'C-m'
+                tmux send-keys 'tail -f /local/syslog/identity-info.log' 'C-m'
 
-		# create pane
-		tmux split-window -t $SESSIONNAME:0 -v
+                # create pane
+                tmux split-window -t $SESSIONNAME:0 -v
 
-		# 2nd pane commands
-		tmux send-keys 'ssh kevinwu@util-d.identity.ucsb.edu' 'C-m'
-		tmux send-keys 'tail -f /local/syslog/php.log' 'C-m'
+                # 2nd pane commands
+                tmux send-keys 'ssh kevinwu@util-d.identity.ucsb.edu' 'C-m'
+                tmux send-keys 'tail -f /local/syslog/php.log' 'C-m'
 
 fi
- 
+
 tmux attach -t $SESSIONNAME
