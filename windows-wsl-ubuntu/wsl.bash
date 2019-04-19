9514dec786c13ee38ae07d305a5b112d66a5b1d3
Kevin Wu <kevin.wu@gmail.com>
2019-04-19 10:03:58 -0700

    add WSL installer notes

---
+++ windows-wsl-ubuntu/wsl.bash
@@ -0,0 +1,12 @@
+# WSL (Ubuntu)
+
+sudo apt update
+sudo apt -y upgrade
+
+# installing NodeJS (https://medium.freecodecamp.org/automating-your-windows-subsystem-linux-setup-df4c9a7b0e7b, https://gist.github.com/noygal/6b7b1796a92d70e24e35f94b53722219)
+
+sudo apt install -y build-essential
+
+# install NVM (https://github.com/creationix/nvm)
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
